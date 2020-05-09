4eb91468138610a4fc3186b5443610e615f74bb0
Abd Asis <abdasis@outlook.com>
2020-05-09 15:10:47 +0700

    updsate form search bar

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -22,13 +22,7 @@
         <nav class="navbar navbar-expand-lg navbar-light p-4" style="background: #fff;" id="top-nav">
             <div class="container">
                 <a class="navbar-brand" href="#">PT MITRA LESTARI ABADI</a>
-                <form class="form-inline my-2 my-lg-0 d-none d-md-block">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"
-                        id="search-form" />
-                    <button class="btn btn-primary  my-2 my-sm-0" type="submit" id="btn-search">
-                        Search
-                    </button>
-                </form>
+
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                   </button>
@@ -60,6 +54,13 @@
                         </li>
                     </ul>
                 </div>
+                <form class="form-inline my-2 my-lg-0 d-none d-md-block">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"
+                        id="search-form" />
+                    <button class="btn btn-primary  my-2 my-sm-0" type="submit" id="btn-search">
+                        Search
+                    </button>
+                </form>
 
             </div>
         </nav>
